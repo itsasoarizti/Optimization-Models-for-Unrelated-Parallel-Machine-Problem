0e3a85fa5431129c6c8f65b689c6626240cd78c0
Itsaso Ariztimuño Cenoz <unitsasoarizti@gmail.com>
2026-05-29 20:33:06 +0200

    Update algoritmo_exacto.py

--- algoritmo_exacto.py
+++ algoritmo_exacto.py
@@ -2,15 +2,14 @@ import numpy as np
 import cvxpy as cv
 import time
 
-def algoritmo_exacto(n,m,C):
+def MILP(n,m,C):
 
-    # Define variables
+    # Variables
 
-    X = [cv.Variable((n+2, n+2), boolean=True) for _m in range(m)] # C-ko matrize bakoitzerako X bat, _m makinak ingoitun tareak eta hauen ordena adierazteute
-    u = cv.Variable(n+2, integer=True)  # makina bakoitzeako tareen orden bat
+    X = [cv.Variable((n+2, n+2), boolean=True) for _m in range(m)]
+    u = cv.Variable(n+2, integer=True)
     J = cv.Variable(m, integer=True)
-    Z = cv.Variable(integer=True)
-    
+    Z = cv.Variable(integer=True)   
     
     ones = np.ones((n+2,1))
     ones2 = np.ones((n+2,1))
@@ -21,15 +20,15 @@ def algoritmo_exacto(n,m,C):
     ones3[n+1] = m
     
 
-    # funcion objetivo
+    # Objective function
 
     objective = cv.Minimize(Z)
 
-    # restrikzioak
+    # Constraints
 
     constraints = []
     
-    # makinak ez iteko egin ezin ditun tareak
+    # so the machine doesn't try to do tasks it can't handle
     
     for _m in range(m):
         for i in range(n+1):
@@ -38,24 +37,23 @@ def algoritmo_exacto(n,m,C):
                     constraints += [X[_m][j,i] == 0]
 
     for _m in range(m):
-        constraints += [X[_m][0,:] @ ones == 1] #0-n hasi
-        constraints += [X[_m][:,n+1] @ ones == 1] #(n+1)-en bukatu
-        constraints += [X[_m][1:-1,:] @ ones <= 1]  # bestetan igual tarea hoi dao igual ez
+        constraints += [X[_m][0,:] @ ones == 1] # starts in 0
+        constraints += [X[_m][:,n+1] @ ones == 1] # finishes in (n+1)
+        constraints += [X[_m][1:-1,:] @ ones <= 1]
         constraints += [X[_m][:,1:-1].T @ ones <= 1]
   
-    # _m i tareara badoa i tareatik atera behar du
+    # if a machine goes to a task, it has to go out from that same task
     for _m in range(m):
         for i in range(1,n+1):
             constraints += [cv.sum(X[_m][:,i]) == cv.sum(X[_m][i,:])]
 
-    # ezin du tarea batetik tarea igualera jun
+    # can't switch from one task to the same task
     for _m in range(m):
         for i in range(n+2):
             constraints += [X[_m][i,i] == 0]  
 
-
     for _m in range(m):
-    # Restricción para evitar subtours en la ruta
+    # prevent soubtours
         for i in range(1, n+1):
             for j in range(1, n+1):
                 if i != j:
@@ -64,15 +62,15 @@ def algoritmo_exacto(n,m,C):
     constraints += [(sum(X[_m] for _m in range(m))) @ ones == ones2]
     constraints += [(sum(X[_m].T for _m in range(m))) @ ones == ones3]
 
-    # J bektorea makina bakoitzak tardatzeun denbora totalakin
+    # J vector: total time of each machine
     for _m in range(m):
         constraints += [cv.sum(cv.multiply(C[_m], X[_m])) == J[_m]]
 
-    # Z = J bektoreko balio altuena
+    # Z value: highest value of J
     for _m in range(m):
         constraints += [Z >= J[_m]]
 
-    # optimizazio problema
+    # Optimization problem
 
     start = time.time()
     prob = cv.Problem(objective, constraints)
@@ -87,20 +85,20 @@ def algoritmo_exacto(n,m,C):
 
     X_sol = [a.tolist() for a in sol]
 
-    # rutak sortu
+    # Create routes
     
-    rutak = []
+    routes = []
 
     for _m in range(len(X_sol)):
-        ruta = [0]
+        route = [0]
         actual = X_sol[_m][0][1]
         while actual !=n+1:
-            ruta.append(actual)
-            indices = np.where(np.array(X_sol[_m])[:, 0] == actual)[0]
-            siguiente = X_sol[_m][indices[0]][1]
-            actual = siguiente
-        ruta.append(n+1)
-        rutak.append(ruta)
+            route.append(actual)
+            indx = np.where(np.array(X_sol[_m])[:, 0] == actual)[0]
+            next = X_sol[_m][indx[0]][1]
+            actual = next
+        route.append(n+1)
+        routes.append(route)
         
 
-    return np.round(Z.value), rutak
+    return np.round(Z.value), routes
